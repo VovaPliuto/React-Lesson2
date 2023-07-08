ae81ef7f9011c1adacf290e50834648ece51eaa6
VovaPliuto <vovaplyuto@gmail.com>
2023-07-08 11:16:55 +0300

    Update task1

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,37 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    searchQuery: '',
+  };
+
+  onInputChange = e => {
+    this.setState({ searchQuery: e.target.value });
+  };
+
+  onSubmitForm = e => {
+    e.preventDefault();
+
+    this.props.onHandleSearch(this.state.searchQuery);
+
+    this.setState({ searchQuery: '' });
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.onSubmitForm}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          onChange={this.onInputChange}
+          placeholder="What do you want to write?"
+          name="search"
+          value={this.state.searchQuery}
+          required
+          autoFocus
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,9 +4,34 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    images: [],
+    searchQuery: '',
+    page: 1,
+  };
+
+  handleSearch = data => {
+    this.setState({ images: [], page: 1, searchQuery: data });
+  };
+
+  handleFetchImages = async () => {
+    const { searchQuery, page } = this.state;
+
+    const data = await ImageService.getImages(searchQuery, page);
+
+    this.setState(prevState => ({
+      images: [...prevState.images, ...data.photos],
+    }));
+  };
+
+  componentDidUpdate() {
+    this.handleFetchImages();
+  }
+
   render() {
     return (
       <>
+        <SearchForm onHandleSearch={this.handleSearch} />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
